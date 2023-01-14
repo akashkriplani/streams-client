b53f6d277786efd9ae36d716b83816340dbcb03b
Akash Kriplani <aakash23kriplani@gmail.com>
2023-01-14 17:14:38 +0530

    Setup redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './components/App';
+import reducers from './reducers';
 
 const el = document.getElementById('root');
 const root = createRoot(el);
 
-root.render(<App />);
+const store = createStore(reducers);
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
