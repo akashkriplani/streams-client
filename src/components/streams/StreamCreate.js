dc9e268e0521e81c12c56e0225dbf5f74da5c61b
Akash Kriplani <aakash23kriplani@gmail.com>
2023-01-15 20:57:39 +0530

    Customizing form fields on stream create component

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -5,15 +5,23 @@ class StreamCreate extends React.Component {
   // The parameter to the rednderInput method is formProps and we extract the input from it.
   // As an alternative, we can use formProps.input instead of just extracting the input field.
   // Console the parameter formProps to know more if you ever visit this page again
-  renderInput({ input }) {
-    return <input {...input} />;
+  renderInput({ input, label }) {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    );
   }
 
   render() {
+    // Anytime a prop like label is passed to the Field component from redux-form package,
+    // It does not know how to make use of it, so it passes that prop (in this case, label)
+    // as a parameter to the renderInput method.
     return (
-      <form>
-        <Field name="title" component={this.renderInput} />
-        <Field name="description" component={this.renderInput} />
+      <form className="ui form">
+        <Field name="title" component={this.renderInput} label="Enter title" />
+        <Field name="description" component={this.renderInput} label="Enter description" />
       </form>
     );
   }
