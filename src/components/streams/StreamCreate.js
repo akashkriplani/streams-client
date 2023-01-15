841abe2bf605f69cf484a3b8ea2f468378239460
Akash Kriplani <aakash23kriplani@gmail.com>
2023-01-15 20:52:45 +0530

    handle events and values on the stream create form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends React.Component {
+  // The parameter to the rednderInput method is formProps and we extract the input from it.
+  // As an alternative, we can use formProps.input instead of just extracting the input field.
+  // Console the parameter formProps to know more if you ever visit this page again
+  renderInput({ input }) {
+    return <input {...input} />;
+  }
+
   render() {
-    return <div>StreamCreate</div>;
+    return (
+      <form>
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
+      </form>
+    );
   }
 }
 
-export default StreamCreate;
+export default reduxForm({ form: 'streamCreate' })(StreamCreate);
