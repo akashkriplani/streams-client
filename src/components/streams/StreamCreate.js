7ba672e6c0f67322da675c711ee729efdc0116f7
Akash Kriplani <aakash23kriplani@gmail.com>
2023-01-15 21:17:05 +0530

    Add validation to the stream create form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -5,11 +5,12 @@ class StreamCreate extends React.Component {
   // The parameter to the rednderInput method is formProps and we extract the input from it.
   // As an alternative, we can use formProps.input instead of just extracting the input field.
   // Console the parameter formProps to know more if you ever visit this page again
-  renderInput({ input, label }) {
+  renderInput({ input, label, meta }) {
     return (
       <div className="field">
         <label>{label}</label>
         <input {...input} />
+        <div>{meta.error}</div>
       </div>
     );
   }
@@ -32,4 +33,20 @@ class StreamCreate extends React.Component {
   }
 }
 
-export default reduxForm({ form: 'streamCreate' })(StreamCreate);
+export const validate = (formValues) => {
+  const errors = {};
+
+  // The 'name' property on the Field component should be used in the error object in order to
+  // show the error message
+  if (!formValues.title) {
+    errors.title = 'You must enter a title';
+  }
+
+  if (!formValues.description) {
+    errors.description = 'You must enter a description';
+  }
+
+  return errors;
+};
+
+export default reduxForm({ form: 'streamCreate', validate: validate })(StreamCreate);
