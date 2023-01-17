48694ef9e965791eeeff1d6383a2c504b2289704
Akash Kriplani <aakash23kriplani@gmail.com>
2023-01-17 22:56:44 +0530

    Fetch the particular stream with id on stream edit component on load

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,5 +1,24 @@
-const StreamEdit = () => {
-  return <div>StreamEdit</div>;
+import React from 'react';
+import { connect } from 'react-redux';
+import { editStream, fetchStream } from '../../actions';
+
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+    return <div>{this.props.stream.title}</div>;
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id]
+  };
 };
 
-export default StreamEdit;
+export default connect(mapStateToProps, { editStream, fetchStream })(StreamEdit);
