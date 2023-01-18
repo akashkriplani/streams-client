2b90543e3592f5d98580436f20dd19ee4f919ea9
Akash Kriplani <aakash23kriplani@gmail.com>
2023-01-18 19:38:50 +0530

    Use stream form component and use it in the stream edit component

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
+import StreamForm from './StreamForm';
 import { editStream, fetchStream } from '../../actions';
 
 class StreamEdit extends React.Component {
@@ -7,11 +9,20 @@ class StreamEdit extends React.Component {
     this.props.fetchStream(this.props.match.params.id);
   }
 
+  onSubmit = (formValues) => {
+    this.props.editStream(this.props.match.params.id, formValues);
+  };
+
   render() {
     if (!this.props.stream) {
       return <div>Loading...</div>;
     }
-    return <div>{this.props.stream.title}</div>;
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamForm initialValues={_.pick(this.props.stream, 'title', 'description')} onSubmit={this.onSubmit} />
+      </div>
+    );
   }
 }
 
