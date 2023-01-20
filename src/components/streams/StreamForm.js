617a3ceed84b57afe2225a87cf3909496b8fbfd7
Akash Kriplani <aakash23kriplani@gmail.com>
2023-01-20 21:12:53 +0530

    Make changes to the reducer and the stream form component to use react final form package

--- src/components/streams/StreamForm.js
+++ src/components/streams/StreamForm.js
@@ -1,22 +1,7 @@
-import React from 'react';
-import { Field, reduxForm } from 'redux-form';
+import { Form, Field } from 'react-final-form';
 
-class StreamForm extends React.Component {
-  // The parameter to the rednderInput method is formProps and we extract the input from it.
-  // As an alternative, we can use formProps.input instead of just extracting the input field.
-  // Console the parameter formProps to know more if you ever visit this page again
-  renderInput = ({ input, label, meta }) => {
-    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
-    return (
-      <div className={className}>
-        <label>{label}</label>
-        <input {...input} autoComplete="off" />
-        {this.renderError(meta)}
-      </div>
-    );
-  };
-
-  renderError({ error, touched }) {
+const StreamForm = (props) => {
+  const renderError = ({ error, touched }) => {
     if (touched && error) {
       return (
         <div className="ui error message">
@@ -24,40 +9,49 @@ class StreamForm extends React.Component {
         </div>
       );
     }
-  }
-
-  onSubmit = (formValues) => {
-    this.props.onSubmit(formValues);
   };
 
-  render() {
-    // Anytime a prop like label is passed to the Field component from redux-form package,
-    // It does not know how to make use of it, so it passes that prop (in this case, label)
-    // as a parameter to the renderInput method.
+  const renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
     return (
-      <form className="ui form error" onSubmit={this.props.handleSubmit(this.onSubmit)}>
-        <Field name="title" component={this.renderInput} label="Enter title" />
-        <Field name="description" component={this.renderInput} label="Enter description" />
-        <button className="ui button primary">Submit</button>
-      </form>
+      <div className={className}>
+        <label>{label}</label>
+        <input {...input} autoComplete="off" />
+        {renderError(meta)}
+      </div>
     );
-  }
-}
+  };
 
-export const validate = (formValues) => {
-  const errors = {};
+  const onSubmit = (formValues) => {
+    props.onSubmit(formValues);
+  };
 
-  // The 'name' property on the Field component should be used in the error object in order to
-  // show the error message
-  if (!formValues.title) {
-    errors.title = 'You must enter a title';
-  }
+  return (
+    <Form
+      initialValues={props.initialValues}
+      onSubmit={onSubmit}
+      validate={(formValues) => {
+        const errors = {};
 
-  if (!formValues.description) {
-    errors.description = 'You must enter a description';
-  }
+        if (!formValues.title) {
+          errors.title = 'You must enter a title';
+        }
 
-  return errors;
+        if (!formValues.description) {
+          errors.description = 'You must enter a description';
+        }
+
+        return errors;
+      }}
+      render={({ handleSubmit }) => (
+        <form onSubmit={handleSubmit} className="ui form error">
+          <Field name="title" component={renderInput} label="Enter Title" />
+          <Field name="description" component={renderInput} label="Enter Description" />
+          <button className="ui button primary">Submit</button>
+        </form>
+      )}
+    />
+  );
 };
 
-export default reduxForm({ form: 'streamForm', validate: validate })(StreamForm);
+export default StreamForm;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,8 @@
 import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
 import streamReducer from './streamReducer';
 
 export default combineReducers({
   auth: authReducer,
-  // the key "form" is mandatory and it should be as is when using the redux form package.
-  form: formReducer,
   streams: streamReducer
 });
